dee7b94f4fb95eb6bd8541c8285114a5bbc9d237
Szymon Grzęda <szymon.grzedi@gmail.com>
2019-02-02 15:26:30 +0100

    Init

---
+++ lucky.py
@@ -0,0 +1,16 @@
+#! python3
+# lucky.py - Opens several Google search results
+
+import requests
+import sys
+import webbrowser
+import bs4
+
+print('Googling...') # display text while downloading the Google page
+res = requests.get('https://google.com/search?q=' + ''.join(sys.argv[1:]))
+res.raise_for_status()
+
+
+# TODO: Retrieve top search result links
+
+# TODO: Open a browser tab for each result
---
+++ mapIt.py
@@ -0,0 +1,16 @@
+#! python3
+# mapIt.py - Launches a map in the browser using and address from the command line or clipboard
+
+import webbrowser
+import sys
+import pyperclip
+if len(sys.argv) > 1:
+    # Get address from command line
+    address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard
+    address = pyperclip.paste()
+# TODO: Get address from clipboard
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
+
