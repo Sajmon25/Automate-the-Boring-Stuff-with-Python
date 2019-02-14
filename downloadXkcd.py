e2bd1c52e68de07f449252d1314299a017554d92
Szymon Grzęda <szymon.grzedi@gmail.com>
2019-02-15 00:07:32 +0100

    Add new part of program. It downloads the image or skip if error occurs

--- downloadXkcd.py
+++ downloadXkcd.py
@@ -10,9 +10,26 @@ os.makedirs('xkcd', exist_ok=True)
 while not url.endswitch('#'):
     # TODO: Download the page
     print('Download page %s...' % url)
-    # TODO: Find the URL of the comic image.
+    res = requests.get(url)
+    res.raise_for_status()
 
-    # TODO: Download the image.
+    soup = bs4.BeautifulSoup(res.text)
+    # TODO: Find the URL of the comic image.
+    comicElem = soup.select('#comic img')
+    if comicElem == []:
+        print('Could not find comic image.')
+    else:
+        try:
+            comicUrl = 'http:' + comicElem[0].get('src')
+            # TODO: Download the image.
+            print('Downloading image %s...' % {comicUrl})
+            res = requests.get(comicUrl)
+            res.raise_for_status()
+        except requests.exceptions.MissingSchema:
+            # skip this comic
+            prevLink = soup.select('a[rel="prev"]')[0]
+            url = 'http://xkcd.com' + prevLink.get('href')
+            continue
 
     # TODO: Save the image to ./xkcd.
 
